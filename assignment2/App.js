2cce218285d0693c849003984647bb7113f9f468
Samarth-Anavatti1 <samarth.anavatti@kyndryl.com>
2023-01-01 18:38:44 +0530

    feat: updated with functional components

--- assignment2/App.js
+++ assignment2/App.js
@@ -1,32 +1,34 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import natureImage from './assets/nature.jpg';
 
-const heading = React.createElement(
-  'h1',
-  {
-    id: 'heading1',
-    class: 'heading',
-  },
-  'This is a react app with parcel bundler'
+const bodyElement = (
+  <div id="container">
+    <h1> This is react app with functional component</h1>
+    <h2> React element is created with JSX</h2>
+  </div>
 );
 
-const heading2 = React.createElement(
-  'h2',
-  {
-    id: 'heading2',
-    class: 'heading2',
-  },
-  'Parcel is the beast'
-);
+const imageElement = <img src={natureImage} alt="nature image" />;
 
-const container = React.createElement(
-  'div',
-  {
-    id: 'container',
-  },
-  [heading, heading2]
+const HeaderComponet = function () {
+  return (
+    <div className="header">
+      <div className="header__label">Namste React</div>
+      <div className="header__about">About</div>
+    </div>
+  );
+};
+
+const LandingComponent = () => (
+  <div>
+    <HeaderComponet />
+    <div className="body_container">
+      {bodyElement}
+      {imageElement}
+    </div>
+  </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(container);
+root.render(<LandingComponent />);
